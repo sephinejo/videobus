86249a0ac23399310ee3f1547d89e4f41fcffd86
SooHyun-Kang <k.josephine19@gmail.com>
2024-01-24 17:20:10 -0800

    implement fake youtube api with axios as class

--- src/pages/VideosList.jsx
+++ src/pages/VideosList.jsx
@@ -1,28 +1,26 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import VideoCard from '../components/VideoCard';
+import FakeYoutube from '../api/fakeYoutube';
 
 function VideosList() {
   const { keyword } = useParams();
   const {
-    isPending,
+    isLoading,
     error,
     data: videos,
   } = useQuery({
     queryKey: ['videos', keyword],
-    queryFn: async () => {
-      const res = await fetch(
-        `/videos/${keyword ? 'search.json' : 'popular.json'}`
-      );
-      const data = await res.json();
-      return data.items;
+    queryFn: () => {
+      const youtube = new FakeYoutube();
+      return youtube.search(keyword);
     },
   });
 
   return (
     <div>
       <h2>Videos {keyword ? keyword : 'Trend Now🔥'}</h2>
-      {isPending && <p>Loading...</p>}
+      {isLoading && <p>Loading...</p>}
       {error && <p>An error has occurred! {error.message}</p>}
       {videos?.map((video) => (
         <VideoCard key={video.id} video={video} />
